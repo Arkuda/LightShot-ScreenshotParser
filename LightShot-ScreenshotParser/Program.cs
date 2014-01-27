81cf859c2a25c7d085f71edab51022b496e08544
Andrey <reyzor2142@gmail.com>
2014-01-27 10:54:52 +0400

    initial

---
+++ LightShot-ScreenshotParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightShot_ScreenshotParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
+
+/*
+  http://prntscr.com/2myyj2   <---- 0AAAA0 
+
+  <img class="image__pic js-image-pic" src="http://i.imgur.com/bOdp5uH.png" alt="">
+ 
+  query - Class="image__pic js-image-pic"
+*/
